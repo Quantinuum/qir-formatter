e65864206ab792da5f38f3634e72ac5175d8ff4c
Kartik Singhal <kartik.singhal@quantinuum.com>
2025-10-21 16:43:27 -0500

    ci: restore some deps for CI script

--- src/scripts/ci_checks.sh
+++ src/scripts/ci_checks.sh
@@ -6,8 +6,8 @@ DIR=$(dirname "$(realpath "$0")")
 SRC_DIR="${DIR}/../qir_formatter"
 
 
-uvx pyright "${SRC_DIR}" "${DIR}/../tests"
-uvx mypy "${SRC_DIR}" "${DIR}/../tests" --namespace-packages
+uv run pyright "${SRC_DIR}" "${DIR}/../tests"
+uv run mypy "${SRC_DIR}" "${DIR}/../tests" --namespace-packages
 
 RUFF_FORMAT_ARGS=()
 RUFF_CHECK_ARGS=()
@@ -17,8 +17,8 @@ else
     RUFF_CHECK_ARGS+=(--fix)
 fi
 
-uvx ruff format "${RUFF_FORMAT_ARGS[@]}" "${SRC_DIR}" "${DIR}/../tests"
-uvx ruff check "${RUFF_CHECK_ARGS[@]}" "${SRC_DIR}" "${DIR}/../tests"
+uv run ruff format "${RUFF_FORMAT_ARGS[@]}" "${SRC_DIR}" "${DIR}/../tests"
+uv run ruff check "${RUFF_CHECK_ARGS[@]}" "${SRC_DIR}" "${DIR}/../tests"
 grep "print(" "${SRC_DIR}" --include="*.py" --recursive --files-with-matches && (echo "Found print statement"; exit 1)
 
 echo "Done"
